aae63b23a313e8a863eb509d80f4ff85deaf47a9
yazac <s1270189@u-aizu.ac.jp>
2026-05-23 16:51:46 +0900

    add kv eventhandler destory method

--- composables/kv/EventHandlers.js
+++ composables/kv/EventHandlers.js
@@ -4,8 +4,14 @@ export class EventHandlers {
     this.postProcessing = postProcessingManager;
     this.ui = uiControls;
     this.canvasWrapper = document.getElementById("canvas-wrapper");
+    this.canvasRect = null;
 
     this.mousemoveScale = 0;
+    this.handleResize = null;
+    this.handleScroll = null;
+    this.handleMouseMove = null;
+    this.handleClick = null;
+    this.canvas = null;
 
     this.setupEventListeners();
   }
@@ -26,30 +32,61 @@ export class EventHandlers {
   }
 
   setupResizeHandler() {
-    window.addEventListener("resize", debounce(200, () => {
+    this.handleResize = debounce(200, () => {
       const newWidth = this.canvasWrapper.clientWidth;
       const newHeight = this.canvasWrapper.clientHeight;
       
       this.sceneManager.resize(newWidth, newHeight);
       this.postProcessing.resize(newWidth, newHeight);
+      this.updateCanvasRect();
 
       this.setupMousemoveScale();
-    }));
+    });
+    window.addEventListener("resize", this.handleResize);
+
+    this.handleScroll = debounce(100, () => {
+      this.updateCanvasRect();
+    });
+    window.addEventListener("scroll", this.handleScroll, { passive: true });
   }
 
   setupMouseEventHandler() {
-    window.addEventListener("mousemove", (e) => {
-      const rect = this.canvasWrapper.getBoundingClientRect();
-      const x = (e.clientX - rect.left) / rect.width * 2 - 1;
-      const y = -(e.clientY - rect.top) / rect.height * 2 + 1;
-      this.sceneManager.updateObject(x*this.mousemoveScale, y*2);
-    });
+    this.updateCanvasRect();
+
+    this.handleMouseMove = (e) => {
+      if (!this.canvasRect || !this.canvasRect.width || !this.canvasRect.height) return;
+      const x = (e.clientX - this.canvasRect.left) / this.canvasRect.width * 2 - 1;
+      const y = -(e.clientY - this.canvasRect.top) / this.canvasRect.height * 2 + 1;
+      this.sceneManager.updateObject(x * this.mousemoveScale, y * 2);
+    };
+
+    window.addEventListener("mousemove", this.handleMouseMove, { passive: true });
   }
 
   setupClickEventHandler() {
-    const canvas = document.getElementById("canvas");
-    canvas.addEventListener("click", () => {
+    this.canvas = document.getElementById("canvas");
+    this.handleClick = () => {
       this.sceneManager.startArukuchanRotation();
-    });
+    };
+    this.canvas.addEventListener("click", this.handleClick);
+  }
+
+  updateCanvasRect() {
+    this.canvasRect = this.canvasWrapper.getBoundingClientRect();
+  }
+
+  destroy() {
+    if (this.handleResize) {
+      window.removeEventListener("resize", this.handleResize);
+    }
+    if (this.handleScroll) {
+      window.removeEventListener("scroll", this.handleScroll);
+    }
+    if (this.handleMouseMove) {
+      window.removeEventListener("mousemove", this.handleMouseMove);
+    }
+    if (this.canvas && this.handleClick) {
+      this.canvas.removeEventListener("click", this.handleClick);
+    }
   }
 }
